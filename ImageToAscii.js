99a2d9995ea30e8cf84093693b2a413bd98c4dd5
codyaal <artyaal00@gmail.com>
2022-10-17 12:46:09 +0100

    Add styling.

--- ImageToAscii.js
+++ ImageToAscii.js
@@ -1,70 +1,89 @@
-// ASCII shades for black background : t P F O Q B A N W M (works best with canvas's default monospace font)
-// ASCII shades for white background : % a t = ! ~ - : . "space" (works best with canvas's default monospace font)
-// 4 shades with only dots and space : space . : ;
+// ASCII shades for black background : t P F O Q B A N W M
+// ASCII shades for white background : % a t = ! ~ - : . `
+// 4 shades with only dots and space : ` . : ;
 
+const ASCIIDOTS = ["`", ".", ":", ";"];
+const ASCIIBLACKBG = ["t", "P", "F", "O", "Q", "B", "A", "N", "W", "M"];
+const ASCIIWHITEBG = ["%", "a", "t", "=", "!", "~", "-", ":", ".", "`"];
+const RESOLUTION = 200;
 
+let inputChanged = false;
 
-const ASCIIDOTS = [' ', '.', ':', ';']
-const ASCIIBLACKBG = ['t', 'P', 'F', 'O', 'Q', 'B', 'A', 'N', 'W', 'M']
-const ASCIIWHITEBG = ['%', 'a', 't', '=', '!', '~', '-', ':', '.', ' ']
-const RESOLUTION = 64
-const CANVASWIDTH = 1000
-const CANVASHEIGHT = 1000
+const drawingBoard = document.getElementById("drawingBoard");
+const submit = document.getElementById("submit");
+const imageInput = document.getElementById("imageInput");
+const imagePreview = document.getElementById("previewImg");
 
-const canvas = document.getElementById("drawingBoard")
-canvas.width = CANVASWIDTH
-canvas.height = CANVASHEIGHT
-const context = canvas.getContext('2d')
-const submit = document.getElementById("submit")
-const imageInput = document.getElementById("imageInput")
-
-
+imageInput.onchange = () => {
+  inputChanged = true;
+  imagePreview.style.backgroundImage =
+    "url('" + window.URL.createObjectURL(imageInput.files[0]) + "')";
+};
 
 submit.onclick = () => {
-    let img = imageInput.files[0]
-    let imgBlob = new Blob([img], {type : 'image/jpeg'})
-    let url = window.URL.createObjectURL(imgBlob)
-    
+  if (inputChanged) {
+    let img = imageInput.files[0];
+    let imgBlob = new Blob([img], { type: "image/jpeg" });
+    let url = window.URL.createObjectURL(imgBlob);
 
-    let imagePreview = document.createElement('img')
-    imagePreview.src = url
+    let image = document.createElement("img");
+    image.src = url;
 
-    //imagePreview.onload = () => context.drawImage(imagePreview,0,0,RESOLUTION,RESOLUTION)
-    drawAscii(imagePreview, ASCIIDOTS)
-
-
-}
-
-function drawAscii(image, asciiShades) {
     image.onload = () => {
-        context.drawImage(image, 0, 0, RESOLUTION, RESOLUTION)
-        let imageData = context.getImageData(0, 0, RESOLUTION, RESOLUTION)
-        let rgbaData = Array.from(imageData.data)
-        let grayScale = []
-        let asciiArray = []
+      drawAsciiOnParagraph(getAsciiArray(image, ASCIIWHITEBG));
+    };
+    inputChanged = false;
+  }
+};
 
-        context.clearRect(0, 0, canvas.width, canvas.height)
+function getAsciiArray(image, asciiShades) {
+  const canvas = document.createElement("canvas");
+  const context = canvas.getContext("2d");
+  canvas.style.position = "fixed";
+  canvas.style.top = 0;
+  canvas.style.left = 0;
+  canvas.width = RESOLUTION;
+  canvas.height = RESOLUTION;
+  context.drawImage(image, 0, 0, RESOLUTION, RESOLUTION);
+  let imageData = context.getImageData(0, 0, RESOLUTION, RESOLUTION);
+  let rgbaData = Array.from(imageData.data);
+  let grayScale = [];
+  let asciiArray = [];
 
-        //getting grayscale values from RGB values : (R+G+B)/3
-        for(let i=0; i<rgbaData.length; i+=4){
-            grayScale.push(Math.round((rgbaData[i] + rgbaData[i+1] + rgbaData[i+2])/3))
-        }
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  canvas.width = 0;
+  canvas.height = 0;
 
-        //calculating the corresponding ASCII characters from grayscale values
-        for(let i=0; i<grayScale.length; i++){
-            let shades = 255/asciiShades.length
-            asciiArray.push(asciiShades[Math.floor(grayScale[i]/shades)])
-        }
+  //getting grayscale values from RGB values : (R+G+B)/3
+  for (let i = 0; i < rgbaData.length; i += 4) {
+    grayScale.push(
+      Math.round((rgbaData[i] + rgbaData[i + 1] + rgbaData[i + 2]) / 3)
+    );
+  }
 
-        context.font = "20px monospace"
-        context.fillStyle = "white"
+  //calculating the corresponding ASCII characters from grayscale values
+  for (let i = 0; i < grayScale.length; i++) {
+    let shades = 255 / asciiShades.length;
+    asciiArray.push(asciiShades[Math.floor(grayScale[i] / shades)]);
+  }
 
-        //drawing ASCII characters on the canvas
-        for(let i=0; i<RESOLUTION; i++){
-            context.fillText(asciiArray.slice(i*RESOLUTION, i*RESOLUTION + RESOLUTION).join(""), 20, 20 + i*15)
-        }
-    }
-
+  return asciiArray;
 }
 
+//draw the ASCII characters as text on an html element
+function drawAsciiOnParagraph(asciiArray) {
+  drawingBoard.innerHTML = "";
+  drawingBoard.style.fontFamily = "monospace";
+  drawingBoard.style.lineHeight = 0.75;
 
+  for (let i = 0; i < RESOLUTION; i++) {
+    drawingBoard.append(
+      document.createTextNode(
+        asciiArray.slice(i * RESOLUTION, i * RESOLUTION + RESOLUTION).join("")
+      )
+    );
+    drawingBoard.append(document.createElement("br"));
+  }
+  drawingBoard.style.width = "fit-content";
+  drawingBoard.style.height = "fit-content";
+}
